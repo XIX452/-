4ae455f045eebe607d2c853668fe16099bc67d80
XIX452 <200106990+XIX452@users.noreply.github.com>
2025-02-20 17:02:15 +0100

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -1,4 +1,8 @@
-
+if _G.UIScriptHasRun then
+    warn("Anti multi execute")
+    return
+end
+_G.UIScriptHasRun = true
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
